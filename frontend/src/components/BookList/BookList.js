5c19a1ee649498781b2b99785589dbe0709758f2
TataRuda <tatarudova.06@gmail.com>
2024-02-29 21:21:25 +0100

    highlight filter book list

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -4,6 +4,7 @@ import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
 import {
   selectTitleFilter,
   selectAuthorFilter,
+  selectOnlyFavoriteFilter,
 } from '../../redux/slices/filterSlice';
 import './BookList.css';
 
@@ -11,6 +12,7 @@ const BookList = () => {
   const books = useSelector((state) => state.books);
   const titleFilter = useSelector(selectTitleFilter);
   const authorFilter = useSelector(selectAuthorFilter);
+  const onlyFavoriteFilter = useSelector(selectOnlyFavoriteFilter);
 
   const dispatch = useDispatch();
   const handleDeleteBook = (id) => {
@@ -28,8 +30,24 @@ const BookList = () => {
     const matchesAuthor = book.author
       .toLowerCase()
       .includes(authorFilter.toLowerCase());
-    return matchesTitle && matchesAuthor;
+    const matchesFavorite = onlyFavoriteFilter ? book.isFavorite : true;
+    return matchesTitle && matchesAuthor && matchesFavorite;
   });
+
+  const highLightMatch = (text, filter) => {
+    if (!filter) return text;
+    const regex = new RegExp(`(${filter})`, 'gi');
+    return text.split(regex).map((substring, i) => {
+      if (substring.toLowerCase() === filter.toLowerCase()) {
+        return (
+          <span key={i} className='highlight'>
+            {substring}
+          </span>
+        );
+      }
+      return substring;
+    });
+  };
 
   return (
     <div className='app-block book-list'>
@@ -41,19 +59,20 @@ const BookList = () => {
           {filteredBooks.map((book, i) => (
             <li key={book.id}>
               <div className='book-info'>
-                {++i}. {book.title} by {book.author}
-                <div className='book-actions'>
-                  <span onClick={() => handleToggleFavorite(book.id)}>
-                    {book.isFavorite ? (
-                      <BsBookmarkStarFill className='star-icon' />
-                    ) : (
-                      <BsBookmarkStar className='star-icon' />
-                    )}
-                  </span>
-                  <button onClick={() => handleDeleteBook(book.id)}>
-                    Delete
-                  </button>
-                </div>
+                {++i}. {highLightMatch(book.title, titleFilter)} by&nbsp;
+                <strong>{highLightMatch(book.author, authorFilter)}</strong>
+              </div>
+              <div className='book-actions'>
+                <span onClick={() => handleToggleFavorite(book.id)}>
+                  {book.isFavorite ? (
+                    <BsBookmarkStarFill className='star-icon' />
+                  ) : (
+                    <BsBookmarkStar className='star-icon' />
+                  )}
+                </span>
+                <button onClick={() => handleDeleteBook(book.id)}>
+                  Delete
+                </button>
               </div>
             </li>
           ))}
--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -4,6 +4,8 @@ import {
   selectTitleFilter,
   setAuthorFilter,
   selectAuthorFilter,
+  setOnlyFavoriteFilter,
+  selectOnlyFavoriteFilter,
   resetFilters,
 } from '../../redux/slices/filterSlice';
 import './Filter.css';
@@ -12,6 +14,7 @@ const Filter = () => {
   const dispatch = useDispatch();
   const titleFilter = useSelector(selectTitleFilter);
   const authorFilter = useSelector(selectAuthorFilter);
+  const onlyFavoriteFilter = useSelector(selectOnlyFavoriteFilter);
 
   const handleTitleFilterChange = (e) => {
     dispatch(setTitleFilter(e.target.value));
@@ -19,6 +22,10 @@ const Filter = () => {
 
   const handleAuthorFilterChange = (e) => {
     dispatch(setAuthorFilter(e.target.value));
+  };
+
+  const handleOnlyFavoriteFilterChange = () => {
+    dispatch(setOnlyFavoriteFilter());
   };
 
   const handleResetFilters = () => {
@@ -42,7 +49,17 @@ const Filter = () => {
             value={authorFilter}
             type='text'
             placeholder='Filter by author...'
-          ></input>
+          />
+        </div>
+        <div className='filter-group'>
+          <label>
+            <input
+              type='checkbox'
+              onChange={handleOnlyFavoriteFilterChange}
+              checked={onlyFavoriteFilter}
+            />
+            Only Favorite
+          </label>
         </div>
         <button type='button' onClick={handleResetFilters}>
           Reset
